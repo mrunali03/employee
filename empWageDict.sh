4e7a62f8652e1e43c12e4fb5dd51dcd6c947396f
mrunali <mrunali.chaudhari1996@gmail.com>
2020-07-03 21:20:36 +0530

    add to employee program

---
+++ empWageDict.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function getWorkHrs(){
+   case $1 in
+   1) empHrs=8;;
+   2) empHrs=4;;
+   *) empHrs=0
+   esac
+   echo $empHrs
+}
+
+declare -A dailyWage
+maxHrs=100
+empRatePerHr=20
+workDays=20
+
+totalEmpHr=0
+totalWorkDays=0
+
+while [ $totalWorkDays -lt $workDays ] && [ $totalEmpHr -lt $maxHrs ]
+do
+   totalWorkDays=`expr $totalWorkDays + 1`
+   workHours="$( getWorkHrs $((RANDOM%3)) )"
+   totalEmpHr=$(($totalEmpHr+$workHours))
+   dailyWage["Day $totalWorkDays"]=$(($workHours * $empRatePerHr))
+done
+
+totalSalary=$(($totalEmpHr*$empRatePerHr))
+echo ${!dailyWage[@]}
+echo ${dailyWage[@]}
