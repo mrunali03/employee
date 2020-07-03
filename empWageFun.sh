f743086a03086a9373c0e9a0b6ef36f09aba1d6c
mrunali <mrunali.chaudhari1996@gmail.com>
2020-07-03 21:25:26 +0530

    commit for program

---
+++ empWageFun.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function getWorkHrs(){
+	case $1 in
+	1) empHrs=8;;
+	2) empHrs=4;;
+	*) empHrs=0
+	esac
+	echo $empHrs
+}
+
+maxHrs=100
+empRatePerHr=20
+workDays=20
+
+totalEmpHr=0
+totalWorkDays=0
+
+while [ $totalWorkDays -lt $workDays ] && [ $totalEmpHr -lt $maxHrs ]
+do
+	totalWorkDays=`expr $totalWorkDays + 1`
+	workHours="$( getWorkHrs $((RANDOM%3)) )"
+	totalEmpHr=$(($totalEmpHr+$workHours))
+done
+
+totalSalary=$(($totalEmpHr*$empRatePerHr))
+echo $totalSalary
